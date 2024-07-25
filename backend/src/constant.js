f1203706c1aebf0949ef33ba0c3cc78bf8e8502a
Nishantsingh11 <ns114046@gmail.com>
2024-07-25 11:54:15 +0530

    inital commit

---
+++ backend/src/constant.js
@@ -0,0 +1,3 @@
+const DBNAME = "CommunityForum"
+
+export { DBNAME }
---
+++ backend/src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import {DBNAME} from "../constant.js";
+
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DBNAME}`
+    );
+    console.log(`MONGODB connected: ${connectInstance.connection.host}`);
+
+  } catch (err) {
+    console.log("MONGODB connection error ", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
