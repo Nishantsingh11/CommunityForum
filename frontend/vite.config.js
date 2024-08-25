4aeb6ed610b8c0dc61c0ad06784c3cbb304e9605
Nishantsingh11 <ns114046@gmail.com>
2024-08-25 20:00:47 +0530

    chore: Update npm dependencies and add vite-plugin-static-copy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',  // Set the base path to relative
   plugins: [react()],
   resolve: {
     alias: {
